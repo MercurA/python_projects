10d86446aac94b81abbbfe8e0c4dcba73df9c25c
MercurA <necrosis.state@gmail.com>
2017-06-14 13:55:38 +0300

    A mini game of Dices

---
+++ dices.py
@@ -0,0 +1,12 @@
+from random import randint
+
+def dice1():
+	val = randint(1,6)
+	return val
+def dice2():
+	val = randint(1,6)
+	return val
+
+
+
+print "Your dice roll is: %d and %d" % (dice1(), dice2())
